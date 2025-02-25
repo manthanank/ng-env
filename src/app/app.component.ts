98ad986e886f516546cb6d62927856dd2313c1a7
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-02-25 11:54:47 +0530

    fix: log fetch call result for API key in constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,6 @@ export class AppComponent {
   protected readonly apiKey = environment.apiKey;
 
   constructor() {
-    fetch(environment.apiKey);
+    console.log(fetch(environment.apiKey));
   }
 }
